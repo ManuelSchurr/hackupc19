c68c038eda422592608b962c62ff124c83faf270
Manuel Schurr <admin@manuelschurr.com>
2019-10-11 23:37:44 +0200

    implement api calls (+gitignore)

--- consume_api.py
+++ consume_api.py
@@ -3,13 +3,13 @@ import json
 
 
 API_KEY = "skyscanner-hackupc2019"
-API_URL = "https://www.skyscanner.net/g/chiron/api/v1/flights/browse/browsequotes/v1.0/"
+API_URL = "https://www.skyscanner.net/g/chiron/api/v1/"
 
 
 headers = {"user-agent": "HackUPC19", "Accept": "application/json", "api-key": API_KEY}
 
-
-def get_cheapest_fare(
+# Contains default testing parameters, can - and should - be overwritten
+def browse_quotes(
     country="DE",
     currency="EUR",
     locale="DE",
@@ -19,10 +19,64 @@ def get_cheapest_fare(
     inbound_partial_date="2019-11-12",
 ):
 
-    req_url = f"{API_URL}{country}/{currency}/{locale}/{origin_place}/{destination_place}/{outbound_partial_date}/{inbound_partial_date}/"
+    req_url = f"{API_URL}/flights/browse/browsequotes/v1.0/{country}/{currency}/{locale}/{origin_place}/{destination_place}/{outbound_partial_date}/{inbound_partial_date}/"
     response = requests.get(req_url, headers=headers)
-    return response.json()
+
+    with open("quotes.json", "w") as outfile:
+        json.dump(response.json(), outfile)
 
 
-print(get_cheapest_fare())
+def browse_routes(
+    country="DE",
+    currency="EUR",
+    locale="DE",
+    origin_place="FRA",
+    destination_place="BER",
+    outbound_partial_date="2019-11-11",
+    inbound_partial_date="2019-11-12",
+):
+    req_url = f"{API_URL}/flights/browse/browseroutes/v1.0/{country}/{currency}/{locale}/{origin_place}/{destination_place}/{outbound_partial_date}/{inbound_partial_date}/"
+    response = requests.get(req_url, headers=headers)
 
+    with open("routes.json", "w") as outfile:
+        json.dump(response.json(), outfile)
+
+
+def browse_dates(
+    country="DE",
+    currency="EUR",
+    locale="DE",
+    origin_place="FRA",
+    destination_place="BER",
+    outbound_partial_date="2019-11-11",
+    inbound_partial_date="2019-11-12",
+):
+
+    req_url = f"{API_URL}/flights/browse/browsedates/v1.0/{country}/{currency}/{locale}/{origin_place}/{destination_place}/{outbound_partial_date}/{inbound_partial_date}/"
+    response = requests.get(req_url, headers=headers)
+
+    with open("dates.json", "w") as outfile:
+        json.dump(response.json(), outfile)
+
+
+def browse_dates_grid(
+    country="DE",
+    currency="EUR",
+    locale="DE",
+    origin_place="FRA",
+    destination_place="BER",
+    outbound_partial_date="2019-11-11",
+    inbound_partial_date="2019-11-12",
+):
+
+    req_url = f"{API_URL}/flights/browse/browsegrid/v1.0/{country}/{currency}/{locale}/{origin_place}/{destination_place}/{outbound_partial_date}/{inbound_partial_date}/"
+    response = requests.get(req_url, headers=headers)
+
+    with open("dates_grid.json", "w") as outfile:
+        json.dump(response.json(), outfile)
+
+
+browse_quotes()
+browse_routes()
+browse_dates()
+browse_dates_grid()
