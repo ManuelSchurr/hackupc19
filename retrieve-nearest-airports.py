a1c80cde2cd2658f74e58f1ea6803b54dec7c8d3
Holger Konstantin Albrecht <holger.albrecht.la@gmail.com>
2019-10-12 09:54:12 +0200

    Refactor: Update to method

--- retrieve-nearest-airports.py
+++ retrieve-nearest-airports.py
@@ -1,16 +1,22 @@
 import requests
 import json
 
-url = "https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius"
 
-querystring = {"radius":"50","lng":"8.71","lat":"49.412222"}
+def getNearestAirport(latitude, longitude, searchRadius):
+    RAPID_API_KEY = "fdd3d5c155msh470d36197a1bfd6p18b3ccjsn83b2a649205a"
+    url = "https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius"
 
-headers = {
-    'x-rapidapi-host': "cometari-airportsfinder-v1.p.rapidapi.com",
-    'x-rapidapi-key': "fdd3d5c155msh470d36197a1bfd6p18b3ccjsn83b2a649205a"
+    querystring = {"radius": searchRadius, "lng": longitude, "lat": latitude}
+
+    headers = {
+        "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
+        "x-rapidapi-key": RAPID_API_KEY,
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
-with open("near_airports.json", "w") as outfile:
+    with open("near_airports.json", "w") as outfile:
         json.dump(response.json(), outfile)
+
+getNearestAirport(49.412222, 8.71, 50)
+
