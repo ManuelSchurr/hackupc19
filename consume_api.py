b640184c9dc2bdad4776eb836a52a90781c97e9d
Holger Konstantin Albrecht <holger.albrecht.la@gmail.com>
2019-10-12 17:56:45 +0200

    Refactor: Moved Functions to main file

--- consume_api.py
+++ consume_api.py
@@ -241,6 +241,36 @@ def find_flight_route(start_airports, destination_airports):
 
     return routes
 
+def startCarHireLiveSession(
+    market = "DE",
+    currency = "EUR",
+    locale = "de-DE",
+    pickupplace  = "50.110924,8.682127-latlong",
+    dropoffplace = "49.4875,8.466-latlong",
+    pickupdatetime = "2019-11-12T10:00", 
+    dropoffdatetime = "2019-11-13T10:00",
+    driverage = "21"
+):
+
+    req_url = f"{API_URL}/carhire/liveprices/v2/{market}/{currency}/{locale}/{pickupplace}/{dropoffplace}/{pickupdatetime}/{dropoffdatetime}/{driverage}"
+    response = requests.post(req_url, headers=headers)
+
+    session_id = response.json()["session_id"]
+
+    return session_id
+
+def getCheapestRide():
+    session_id = startCarHireLiveSession()
+
+    req_url = f"{API_URL}/carhire/liveprices/v2/?session_id={session_id}"
+    response = requests.get(req_url, headers=headers)
+
+    rides = []
+
+    rides.append({"From": "start", "To": "Destination", "Ride": response.json()["cars"][0]})
+
+    with open ("rides.json", "w") as outfile:
+        json.dump(rides, outfile)
 
 # find_flight_route(["FRA", "STR"], ["TXL", "MUC"])
 
