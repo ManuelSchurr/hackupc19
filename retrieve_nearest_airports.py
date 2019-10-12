3c7fa95f93584f85639d1da5964ef97cd47a022a
Holger Konstantin Albrecht <holger.albrecht.la@gmail.com>
2019-10-12 15:34:20 +0200

    Refactor: Duplicated coordinate method to achieve different outputs

--- retrieve_nearest_airports.py
+++ retrieve_nearest_airports.py
@@ -21,12 +21,31 @@ def getCoordinates(city):
     Lat = response.json()['results'][0]['geometry']['lat']
     Long = response.json()['results'][0]['geometry']['lng']
 
+    return f"{Lat},{Long}"
+
+
+def getCoordinatesSkyscanner(city):
+    url = "https://opencage-geocoder.p.rapidapi.com/geocode/v1/json"
+    OPENCAGE_KEY = "aab438e42c9b4046a5aa99173ec4551e"
+
+    querystring = {"language":"en","key":OPENCAGE_KEY,"q":city}
+
+    headers = {
+        'x-rapidapi-host': "opencage-geocoder.p.rapidapi.com",
+        'x-rapidapi-key': "fdd3d5c155msh470d36197a1bfd6p18b3ccjsn83b2a649205a"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    Lat = response.json()['results'][0]['geometry']['lat']
+    Long = response.json()['results'][0]['geometry']['lng']
+
     return f"{Lat},{Long}-latlong"
 
 # calls getCoordinates which translates the City Name into coordinates
 # takes coordiantes and searchRadius to output nearestAirports
 def getNearestAirport(city, searchRadius):
-    airportCoordinates = getCoordinates(city)
+    airportCoordinates = getCoordinatesSkyscanner(city)
     lat = airportCoordinates.partition(',')[0]
     lng = airportCoordinates.partition(',')[2].partition('-')[0]
     RAPID_API_KEY = "fdd3d5c155msh470d36197a1bfd6p18b3ccjsn83b2a649205a"
